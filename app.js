082b29e2b1ec17fe5663fa708a8bbd1999ae092b
kellumdavis <kellumdavis@gmail.com>
2022-04-08 15:32:49 -0400

    late afternoon push-Mapped values to be able to compare card values and match winners

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+let drawnCard1, drawnCard2;
 
 //Grabbing Elements
 const open = document.getElementById('openModal');
@@ -6,6 +6,10 @@ const modal = document.getElementById('modal')
 const close = document.getElementById('close')
 const cardCountOne = document.querySelector('.playerOneCardCount')
 const cardCountTwo = document.querySelector('.playerTwoCardCount')
+const drawButtonOne = document.querySelector('.drawButtonOne')
+const drawButtonTwo = document.querySelector('.drawButtonTwo')
+const drawCard1 = document.querySelector('.drawCard1')
+const drawCard2 = document.querySelector('.drawCard2')
 //Functions
 const openModal = () => {
   modal.style.display = 'block';
@@ -20,28 +24,55 @@ const closeModal = () => {
 
 // close.addEventListener('click', closeModal)
 
-// startButton.addEventListener('click', (event) => {
-//     event.preventDefault()
-//     shuffle(deck);
-//     splitDeck();
-//     window.location = "index.html";
-// })
+drawButtonOne.addEventListener('click', () => {
+   drawnCard1 = playerOneDeck.pop()
+   drawCard1.innerHTML = drawnCard1.rank + drawnCard1.suit
+   checkWinner()
+   cardCountOne.innerHTML += playerOneDeck.length
+} )
+drawButtonTwo.addEventListener('click', () => {
+    drawnCard2 = playerTwoDeck.pop()
+    drawCard2.innerHTML = drawnCard2.rank + drawnCard2.suit
+    checkWinner()
+     
+ })
 
 //create initial 52 card deck
 let suits = ["♠", "♣", "♥", "♦"];
 let ranks = ['2','3','4','5','6','7','8','9','T','J','Q','K','A'];
+let value = {
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    T: 10,
+    J: 11,
+    Q: 12,
+    K: 13,
+    A: 14
+}
 let deck = [];
 let playerOneDeck = [];
 let playerTwoDeck = [];
 
 for(i = 0;i < suits.length;i++) {
   for(j = 0; j < ranks.length; j++){
-      let card = suits[i] +  ranks[j]
+      let card = {
+          suit: suits[i],
+          rank: ranks[j],
+          value: value[ranks[j]] 
+      }
       deck.push(card)
   }
 }
 shuffle(deck);
 splitDeck();
+
+
 // console.log(deck)
 
 
@@ -75,3 +106,15 @@ function shuffle(deck) {
     console.log(playerTwoDeck)
   }
   
+function checkWinner(){
+    if(drawnCard1.value > drawnCard2.value){
+        console.log('player 1 wins')
+        playerOneDeck.push(drawnCard1,drawnCard2)
+        console.log(playerOneDeck.length)
+    }else if(drawnCard1.value < drawnCard2.value){
+        console.log('player 2 wins')
+    }else{
+        console.log('Draw')
+    }
+}
+ 
